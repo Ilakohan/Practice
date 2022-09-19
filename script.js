21b656934f14328e4db1a36f3fc89f4b31a8ec66
ilakohan <ilakohansal@gmail.com>
2022-09-19 19:55:15 +0200

    add java

---
+++ script.js
@@ -0,0 +1,9 @@
+/*let name = "Ila";
+let otherName = "Marija";
+alert(name);
+alert(otherName);
+alert("Hello " + name + ", " + otherName + ". How was your day?");*/
+
+document.getElementById("footer").innerHTML = "Who Made this page?";
+document.getElementById("footer").style.color = "black";
+document.getElementById("footer").style.backgroundColor = "yellow";
