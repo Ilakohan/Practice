268b2ff006c4063c4b79a93b77b1bc05a0f13f4b
ilakohan <ilakohansal@gmail.com>
2022-09-19 23:38:46 +0200

    fix-topnav

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ alert(name);
 alert(otherName);
 alert("Hello " + name + ", " + otherName + ". How was your day?");*/
 
-document.getElementById("footer").innerHTML = "Who Made this page?";
+/*document.getElementById("footer").innerHTML = "Who Made this page?";
 document.getElementById("footer").style.color = "black";
-document.getElementById("footer").style.backgroundColor = "yellow";
+document.getElementById("footer").style.backgroundColor = "yellow";*/
