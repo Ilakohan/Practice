da8a9e4d6981fb847fc4e6359b5373d0bbbc7f2b
ilakohan <ilakohansal@gmail.com>
2022-09-20 11:21:27 +0200

    java

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*let name = "Ila";
-let otherName = "Marija";
+let otherName = "How are you? ";
 alert(name);
 alert(otherName);
 alert("Hello " + name + ", " + otherName + ". How was your day?");*/
@@ -7,3 +7,24 @@ alert("Hello " + name + ", " + otherName + ". How was your day?");*/
 /*document.getElementById("footer").innerHTML = "Who Made this page?";
 document.getElementById("footer").style.color = "black";
 document.getElementById("footer").style.backgroundColor = "yellow";*/
+
+/*function sayHello() {
+    let myElementById ("content").appendenChild(h2)
+    myElementById.innerHTML = "hello function"
+    document.getElementById ("content").appendChild(myElement)
+
+}
+sayHello();
+sayHello();*/
+
+/*let button =document.getElementById(myCoolButtom)
+button.addEventListener ("click", SayHello);*/
+
+/*function sayHello() {
+  let myElement = document.createElement("h1");
+  myElement.innerHTML = "Thank you for Watching";
+  document.getElementById("footer").appendChild(myElement);
+}
+
+let button = document.getElementById("buttom");
+button.addEventListener("click", sayHello);*/
