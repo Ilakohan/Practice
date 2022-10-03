b38002b8e93e5189d5a9080e23b6ddd6ca074fd9
ilakohan <ilakohansal@gmail.com>
2022-10-04 00:15:02 +0200

    active JS

--- script.js
+++ script.js
@@ -20,11 +20,11 @@ sayHello();*/
 /*let button =document.getElementById(myCoolButtom)
 button.addEventListener ("click", SayHello);*/
 
-/*function sayHello() {
+function sayHello() {
   let myElement = document.createElement("h1");
-  myElement.innerHTML = "Thank you for Watching";
+  myElement.innerHTML = "Ila Kohansal";
   document.getElementById("footer").appendChild(myElement);
 }
 
 let button = document.getElementById("buttom");
-button.addEventListener("click", sayHello);*/
+button.addEventListener("click", sayHello);
