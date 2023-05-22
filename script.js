d407fb50379da9fc2fb23a2560053d4084fa6498
Ila Kohan <ilakohansal@gmail.com>
2023-05-22 19:27:54 +0200

    animation

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-/*let name = "Ila";
-let otherName = "How are you? ";
-alert(name);
-alert(otherName);
-alert("Hello " + name + ", " + otherName + ". How was your day?");*/
-
-/*document.getElementById("footer").innerHTML = "Who Made this page?";
-document.getElementById("footer").style.color = "black";
-document.getElementById("footer").style.backgroundColor = "yellow";*/
-
-/*function sayHello() {
-    let myElementById ("content").appendenChild(h2)
-    myElementById.innerHTML = "hello function"
-    document.getElementById ("content").appendChild(myElement)
-
-}
-sayHello();
-sayHello();*/
-
-/*let button =document.getElementById(myCoolButtom)
-button.addEventListener ("click", SayHello);*/
-
 function sayHello() {
   let myElement = document.createElement("h1");
   myElement.innerHTML = "Ila Kohansal";
